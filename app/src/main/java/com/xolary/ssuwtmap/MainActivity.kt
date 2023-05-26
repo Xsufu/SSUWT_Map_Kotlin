e6b2475f72c731742855a90abf24320298c2291d
Xsufu <aluno.macarrao@gmail.com>
2023-05-26 18:47:36 +0700

    Вывод работы QR-сканера (не работает как надо)

--- app/src/main/java/com/xolary/ssuwtmap/MainActivity.kt
+++ app/src/main/java/com/xolary/ssuwtmap/MainActivity.kt
@@ -1,11 +1,88 @@
 package com.xolary.ssuwtmap
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.commit
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.xolary.ssuwtmap.databinding.ActivityMainBinding
+import com.xolary.ssuwtmap.laborathory.LaboratoryBuilding
+import com.xolary.ssuwtmap.mainBuilding.MainBuilding
+import com.xolary.ssuwtmap.second.SecondBuilding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.navController
+        setupWithNavController(binding.bottomNavigationView, navController)
+
+        val requestCamera = registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) {
+            if (it) {
+                startActivity(Intent(this, ScannerActivity::class.java))
+            } else {
+                Toast.makeText(
+                    applicationContext,
+                    "Отказано в доступе к камере",
+                    Toast.LENGTH_SHORT
+                )
+                    .show()
+            }
+        }
+
+        binding.qrScanner.setOnClickListener{
+            requestCamera.launch(android.Manifest.permission.CAMERA)
+        }
+
+        val buildNumber = intent.getStringExtra("buildNumber")?.toInt()
+        val floor = intent.getStringExtra("floor")?.toInt()
+
+        Log.d("buildNumberLog", buildNumber.toString())
+
+        when(buildNumber) {
+            1 -> {
+                supportFragmentManager.commit {
+                    add(R.id.fragmentContainerView, MainBuilding.newInstance())
+                    setReorderingAllowed(true)
+                    addToBackStack(null)
+                }
+            }
+
+            2 -> {
+                supportFragmentManager.commit {
+                    add(R.id.fragmentContainerView, LaboratoryBuilding.newInstance())
+                    setReorderingAllowed(true)
+                    addToBackStack(null)
+                }
+
+            }
+
+            3 -> {
+                supportFragmentManager.commit {
+                    add(R.id.fragmentContainerView, SecondBuilding.newInstance())
+                    setReorderingAllowed(true)
+                    addToBackStack(null)
+                }
+            }
+            else -> {
+                Toast.makeText(this, "Ошибка", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
